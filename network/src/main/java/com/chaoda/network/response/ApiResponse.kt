7c0fa77b51e2d40ad2f0b1f0e2753df2a76c5065
nichaoda <nichaoda@qq.com>
2021-08-16 17:41:03 +0800

    Retrofit封装：返回结果封装,其中isSuccessful需要根据服务端给出的要求修改

---
+++ network/src/main/java/com/chaoda/network/response/ApiResponse.kt
@@ -0,0 +1,23 @@
+package com.chaoda.network.response
+
+open class ApiResponse<T>(
+    open val code: Int? = null,
+    open val data: T? = null,
+    open val message: String? = null,
+    open val throwable: Throwable? = null
+) {
+    /**
+     * 业务方面请求结果,具体根据服务端的API文档修改
+     */
+    val isSuccessful: Boolean by lazy { code == 0 }
+}
+
+class EmptyApiResponse<T> : ApiResponse<T>()
+
+data class SuccessfulApiResponse<T>(override val data: T) : ApiResponse<T>(data = data)
+
+data class FailedApiResponse<T>(override val code: Int?, override val message: String?) :
+    ApiResponse<T>(code, message = message)
+
+data class ErrorApiResponse<T>(override val throwable: Throwable) :
+    ApiResponse<T>(throwable = throwable)
