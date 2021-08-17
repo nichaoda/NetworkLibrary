d178de8d9b83e8ff01a17411ed64c332968b39d5
nichaoda <nichaoda@qq.com>
2021-08-17 14:54:47 +0800

    网络库使用

--- app/src/main/java/com/chaoda/networklibrary/application/NetworkApplication.kt
+++ app/src/main/java/com/chaoda/networklibrary/application/NetworkApplication.kt
@@ -1,12 +1,3 @@
 package com.chaoda.networklibrary.application
 
-import android.app.Application
-import com.chaoda.network.RetrofitUtils
-import com.chaoda.networklibrary.config.NetworkConfig
-
-class NetworkApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        RetrofitUtils.initConfig(this, NetworkConfig())
-    }
-}
+class NetworkApplication : com.chaoda.network.application.NetworkApplication()
